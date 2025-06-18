28a2092506a7f246f1cdf02f60a8623d34fc3d43
Tautis <69822476+Tautis@users.noreply.github.com>
2025-06-18 20:55:34 +0300

    .

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBpJN7v8c2vxcjeOnFdw4JIfGGH5yYAf2s",
   authDomain: "cryptodash-f61fb.firebaseapp.com",
   projectId: "cryptodash-f61fb",
-  storageBucket: "cryptodash-f61fb.appspot.com", // fixed typo here
+  storageBucket: "cryptodash-f61fb.appspot.com",
   messagingSenderId: "1052240888504",
   appId: "1:1052240888504:web:b482c4d94bfb7ce2c8ea4d",
   measurementId: "G-25E64J8TE5",
