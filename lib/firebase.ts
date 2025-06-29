7738e7e42e4ff33e1187e3363dd7b245c23abb90
Tautis <69822476+Tautis@users.noreply.github.com>
2025-06-29 12:39:37 +0300

    config data
    Moved to an env file

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,15 +2,15 @@ import { getApps, initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Firebase config
+// Firebase config from environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyBpJN7v8c2vxcjeOnFdw4JIfGGH5yYAf2s",
-  authDomain: "cryptodash-f61fb.firebaseapp.com",
-  projectId: "cryptodash-f61fb",
-  storageBucket: "cryptodash-f61fb.appspot.com",
-  messagingSenderId: "1052240888504",
-  appId: "1:1052240888504:web:b482c4d94bfb7ce2c8ea4d",
-  measurementId: "G-25E64J8TE5",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID!,
 };
 
 // Initialize Firebase app
